a7f1c9f4f09a23e92661e9dbf01ee1f52626b677
TOBENTOch <info@tobento.ch>
2024-07-20 10:10:54 +0200

    replacing css root vars

--- src/ViewRenderer.php
+++ src/ViewRenderer.php
@@ -80,10 +80,64 @@ class ViewRenderer implements RendererInterface
             return $html;
         }
         
+        if (str_contains($css, ':root')) {
+            $css = $this->replaceRootVars($css);
+        }
+        
         // Convert css to inline styles:
         return (new CssToInlineStyles())->convert(
             $html,
             $css
         );
+    }
+    
+    /**
+     * Returns the css with the replaced root vars.
+     *
+     * @param string $css
+     * @return string
+     */
+    protected function replaceRootVars(string $css): string
+    {
+        $variables = $this->collectVariables($css);
+        /*$variables = [
+            '--font-primary' => 'Georgia, "Times New Roman", Times, serif',
+        ];*/
+        
+        // replace variables:
+        $callback = function (array $match) use ($variables): string {
+            if (array_key_exists($match[1], $variables)) {
+                return $variables[$match[1]];
+            }
+            
+            return $match[0];
+        };
+		
+        $css = preg_replace_callback('/var\((--[a-zA-Z0-9-_]+)(?:\)|,\s*(.*)\))/', $callback, $css);
+        
+        // replaces :root vars:
+        $css = preg_replace('/--[a-zA-Z0-9-_]+[:](.*)\;/', '', $css);
+        
+        return $css;
+    }
+    
+    /**
+     * Returns the collected css variables.
+     *
+     * @param string $css
+     * @return array
+     */
+    protected function collectVariables(string $css): array
+    {
+        preg_match_all('/--[a-zA-Z0-9-_]+[:](.*)\;/', $css, $matches);
+        
+        $variables = [];
+        
+        foreach($matches[0] ?? [] as $match) {
+            $data = explode(':', $match);
+            $variables[$data[0]] = rtrim($data[1], ';');
+        }
+
+        return $variables;
     }
 }
